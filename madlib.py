7184fd20f1360b78fa334153a8d66845111172ce
YGSDeepshikha <deepshikhayechuri@gmail.com>
2026-07-11 20:36:51 +0530

    Create madlib.py

---
+++ madlib.py
@@ -0,0 +1,12 @@
+print("Welcome to python madlib game!")
+print("Fill in the blanks by giving the input and create your own fun story")
+
+name = input("name: ")
+place = input("place: ")
+adjective = input("adjective: ")
+animal = input("animal: ")
+verb = input("verb: ")
+food = input("food: ")
+
+Story = f" THE GREAT ADVENTURE
+On a pleasant evening, {name} decided to vist {place}. While walking, {name} found a {adjective} {animal} happily {verb}. Feeling curiours, {name} shared {food} with {aniaml}. the{animal] smiled and gladly accepted it. Everyone was suprised since the {animal} is always overconcious and guarded against everyone. From that onwards the people in {place} recongised {name} for the friendly behaviour. 
